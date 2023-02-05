5bcbbb89c3160134095ad256b7f9ee0f6f80c806
Soikat <mehfuzahmedbd@gmail.com>
2023-02-05 23:52:04 +0600

    live test problem solved

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Hello soikat";
+$marks=10;
+
+if($marks>=60){
+echo(" the grade will be First Division");
+}elseif($marks <=59  &&  $marks>=45){
+echo(" the grade will be Second Division");
+}elseif($marks <=44  &&  $marks >=33){
+echo(" the grade will be Third Division");
+}else{
+echo(" the student will  Fail");
+}
